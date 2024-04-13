9dcf1dfe34f092d0390e23e40d5138935707277a
Iñigo Quintana <quintanadinigo@gmail.com>
2024-04-13 13:29:05 -0600

    update diary

--- web/src/pages/diary/diary.tsx
+++ web/src/pages/diary/diary.tsx
@@ -1,38 +1,17 @@
 import PageTemplate from "@assets/PageTemplate";
 import whiteBack from "@assets/fondoT.jpg";
 
-const Home = () => {
-    const containerStyles = {
-        backgroundImage: `url(${whiteBack})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        padding: "200px",
-      
-    };
-
-    const contentStyles = {
-        textAlign: "center",
-        color: "black",
-        fontSize: 39,
-    };
-
-    const titleStyles = {
-        textAlign: "center",
-        color: "black",
-        fontSize: 60, 
-      };
-
+const Diary = () => {
     return (
         <PageTemplate>
-            <div style={containerStyles}>
-                <div style={contentStyles}>
-                    <h1 style={titleStyles}><b>¿Qué es Diary?</b></h1>
-                    <p>Diary es una plataforma diseñada para fomentar el <b>bienestar mental </b> y <b>emocional</b>. Nuestro objetivo es proporcionar a los usuarios una herramienta
-                    práctica para <b>registrar</b> y <b>comprender</b> sus emociones diarias, así como para facilitar la comunicación y colaboración con sus profesionales de la <b>salud mental</b>.</p>
+            <div className="bg-cover bg-center" style={{backgroundImage: `url(${whiteBack})`}}>
+                <div className="p-8 text-center text-black">
+                    <h1 className="text-6xl font-bold mb-8">¿Qué es Diary?</h1>
+                    <p>Diary es una plataforma diseñada para fomentar el <b>bienestar mental</b> y <b>emocional</b>. Nuestro objetivo es proporcionar a los usuarios una herramienta práctica para <b>registrar</b> y <b>comprender</b> sus emociones diarias, así como para facilitar la comunicación y colaboración con sus profesionales de la <b>salud mental</b>.</p>
                 </div>
             </div>
         </PageTemplate>
     );
 }
 
-export default Home;
+export default Diary;
