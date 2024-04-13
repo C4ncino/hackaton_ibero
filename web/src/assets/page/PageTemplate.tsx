80f1dce6461445e916914bc2555b4f721d11cb38
C4ncino <193305@iberopuebla.mx>
2024-04-13 14:12:58 -0600

    Remake landin Page

--- web/src/assets/page/PageTemplate.tsx
+++ web/src/assets/page/PageTemplate.tsx
@@ -4,12 +4,14 @@ import Footer from "./Footer";
 
 type Props = {
     children: React.ReactNode,
+    bg?: string
+    needBlur?: boolean
 }
 
-const PageTemplate = ({ children }: Props) => {
+const PageTemplate = ({ children, bg, needBlur }: Props) => {
     return (
-        <div className="bg-no-repeat bg-scroll bg-cover bg-center bg-[url(https://img.freepik.com/vector-gratis/fondo-acuarela-abstracta-acuarela-pintada-mano_23-2149018547.jpg?w=1060&t=st=1713026988~exp=1713027588~hmac=34f17a301be45cf39ccc6a3c2a2c849177d2956f8dffe472b18116f1fd278740)] flex flex-col min-h-screen">
-            <Navbar />
+        <div className="bg-no-repeat bg-scroll bg-cover bg-center flex flex-col min-h-screen" style={{ backgroundImage: `url(${bg ? bg : 'https://img.freepik.com/vector-gratis/fondo-acuarela-abstracta-acuarela-pintada-mano_23-2149018547.jpg?w=1060&t=st=1713026988~exp=1713027588~hmac=34f17a301be45cf39ccc6a3c2a2c849177d2956f8dffe472b18116f1fd278740'})` }}>
+            <Navbar needBlur={needBlur} />
 
             <main className="grow ">
                 {children}
